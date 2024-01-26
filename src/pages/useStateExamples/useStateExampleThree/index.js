e491637d75ffce500f3af4252b057c6959379b39
Christian <bournecreative@gmail.com>
2024-01-26 00:49:08 -0600

    create useState example three - illustrates multiple props within 1 state object

---
+++ src/pages/useStateExamples/useStateExampleThree/index.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Card, Col, Row, Tag, Button } from "antd";
+export const UseStateExampleThree = () => {
+  const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
+  const codeExample = `const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
+  ...code  
+  <Button type="primary" onClick={() => setCount((c) => ({ ...c, count1: c.count1 + 1 }))}>+1</Button>`;
+  return (
+    <Row gutter={16}>
+      <Col span={24}>
+        <Card bordered={false}>
+          <h2>
+            Multiple states <Tag color="blue">useState</Tag>
+          </h2>
+          <p>
+            We can multiple state values using numerous useState hooks. But we
+            can also combine states into a single useState object if it makes
+            sense.
+          </p>
+          <h3>Example</h3>
+          <p>
+            Here we have destructured our state into 2 properties, setting them
+            with different initial values. Depending on our end-goal we can
+            choose to update both state properties or just one as we have
+            demonstrated here.
+          </p>
+          <pre>`${codeExample}`</pre>
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card title="Count1" bordered={false}>
+          <span>Count1: {count1}</span>
+          <div>
+            <Button
+              type="primary"
+              onClick={() => setCount((c) => ({ ...c, count1: c.count1 + 1 }))}
+            >
+              +1
+            </Button>
+          </div>
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card title="Count2" bordered={false}>
+          <span>Count2: {count2}</span>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
