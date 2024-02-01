710f1f3c14591cbdf8a2ee48f81fb4bcda66d208
Christian <bournecreative@gmail.com>
2024-01-31 20:35:47 -0600

    implement example of useCallMemo to illustrate how we can avoid re-computing expensive functions

--- src/pages/useMemoExamples/useMemoExample1/useMemoExample1.jsx
+++ src/pages/useMemoExamples/useMemoExample1/useMemoExample1.jsx
@@ -1,3 +1,41 @@
+import { useState, useMemo, useCallback } from "react";
+import { Button } from "antd";
+import { useFetch } from "../../../hooks/useFetch";
+
 export const UseMemoExample1 = () => {
-  return <div>Test</div>;
+  const [count, setCount] = useState(0);
+  const [longestWord, setLongestWord] = useState(null);
+
+  const { data } = useFetch(
+    "https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json"
+  );
+
+  const computeLongestWord = useCallback((arr) => {
+    if (!arr) {
+      return [];
+    }
+    let longestWord = "";
+    arr.forEach((sentence) => {
+      sentence.split(" ").forEach((word) => {
+        if (word.length > longestWord.length) {
+          longestWord = word;
+        }
+      });
+    });
+    console.log("Check, Did computelongestWord run", longestWord);
+    return longestWord;
+  }, []);
+
+  const memoLongestWord = useMemo(
+    () => computeLongestWord(data),
+    [computeLongestWord, data]
+  );
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <div>{memoLongestWord}</div>
+      <Button onClick={() => setCount((c) => c + 1)}>Increment++</Button>
+    </div>
+  );
 };
